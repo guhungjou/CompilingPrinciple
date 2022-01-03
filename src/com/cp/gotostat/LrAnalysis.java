080c271915e54a8b2b8d06489401916a9f0ddce8
guhungjou <1213618635@qq.com>
2022-01-03 11:34:16 +0800

    Revert "Revert "3.9修改""
    This reverts commit 3ea5c33997807a6435cbd17f24966c2ce7fb958d.

--- src/com/cp/gotostat/LrAnalysis.java
+++ src/com/cp/gotostat/LrAnalysis.java
@@ -13,13 +13,17 @@ import java.util.*;
  * Describe:
  * 2022.1.3 03:00备注
  * 文件路径自行改动
- * 测试样例为ppt“自上而下语法分析”第112页、本项目goto代码（goto的action表有问题，分析时扫描结果为error）
+ * 测试样例为ppt“自上而下语法分析”第112页、本项目goto代码
  */
 public class LrAnalysis {
-    String fileAction = "src/com/cp/gotostat/action.txt";
-    String fileGoto="src/com/cp/gotostat/goto.txt";
-    String testCase="src/com/cp/gotostat/例题输入ppt112页.txt";
-    String wenfa="src/com/cp/gotostat/wenfa.txt";
+//    String fileAction = "src/com/cp/gotostat/action.txt";
+    String fileAction = "Z:\\test\\action.txt";
+//    String fileGoto="src/com/cp/gotostat/goto.txt";
+    String fileGoto="Z:\\test\\goto.txt";
+    String testCase="Z:\\test\\test-case\\goto.txt";
+//    String testCase="src/com/cp/gotostat/例题输入ppt112页.txt";
+    String wenfa="Z:\\test\\wenfa.txt";
+//    String wenfa="src/com/cp/gotostat/wenfa.txt";
     @Test
    public void myAnalysis() throws FileNotFoundException {
         LRTable lrTable=new LRTable();
@@ -97,92 +101,98 @@ public class LrAnalysis {
             }
         }
         System.out.println("==========");
+        List<StringBuilder> list=new ArrayList<>();
         try (Scanner sc = new Scanner(new FileReader(testCase))) {
 //            StringBuilder sb=new StringBuilder("");
             sc.useDelimiter("\r\n");
-            StringBuilder sb=new StringBuilder();
 //            Integer ii=1;
             while (sc.hasNext()) {   //按分隔符读取字符串
-
+                StringBuilder sb = new StringBuilder();
                 String str = sc.next();
 //                System.out.print(str);
 
 //                if(str.contains("")){
 //                    continue;
 //                }
-                if(str.contains("//")){
+                if (str.contains("//")) {
                     String[] split = str.split("//");
                     sb.append(split[0]);
 //                    System.out.println(sb);
-                }else{
+                } else {
                     sb.append(str);
                 }
+                list.add(sb);
 //                System.out.println("!11"+sb);
 //                System.out.println(ii.toString()+sb);
 //                ii++;
-              }
-            System.out.println("测试输入："+sb.toString());
-            String[] s = sb.toString().split(" ");
+            }
+            int jj=0;
+            for (StringBuilder sb : list) {
+                jj++;
+                System.out.println("("+jj+") 测试输入：" + sb.toString());
+                String[] s = sb.toString().split(" ");
 //            System.out.println(s[3]);
-            Stack<Integer> stackState=new Stack<>();
-            Stack<String> stack1=new Stack<>();
-            Stack<String> stack2=new Stack<>();
+                Stack<Integer> stackState = new Stack<>();
+                Stack<String> stack1 = new Stack<>();
+                Stack<String> stack2 = new Stack<>();
 //            Map<String, List<Integer>> act = lrTable.getAction();
 //            Map<String, List<Integer>> go = lrTable.getGoto();
-            stack1.push("#");
-            stack2.push("#");
+                stack1.push("#");
+                stack2.push("#");
                 stackState.push(0);
-            for(int i=s.length-1;i>=0;i--) {
-                stack2.push(s[i]);
-            }
-            System.out.println("==========");
+                for (int i = s.length - 1; i >= 0; i--) {
+                    stack2.push(s[i]);
+                }
+                System.out.println("==========");
 //            System.out.println("a的5为："+action.get("a").get(5));
-            System.out.println("LR分析表扫描结果：");
+                System.out.println("LR分析表扫描结果：");
 
-            do{
+                do {
 //                System.out.println(stackState);
-                String input = stack2.peek();
+                    String input = stack2.peek();
 //                System.out.println("输入："+input);
 
-                Integer peek = stackState.peek();
+                    Integer peek = stackState.peek();
 //                System.out.println("状态栈顶："+peek);
-                Integer canshu = action.get(input).get(peek);
+                    Integer canshu = action.get(input).get(peek);
 //                System.out.println(action.get(input));
 //                System.out.println("加入："+canshu);
-                if(canshu>0&&canshu<999){
-                    stackState.push(canshu);
-                    if(!stack2.peek().equals("#"))
-                        stack2.pop();
-                    System.out.println("移入："+input);
-                    if(!input.equals("#"))
-                      stack1.push(input);
-                }else if(canshu<0){
-                    String wf = wenfaList.get(-canshu);
+                    if (canshu > 0 && canshu < 999) {
+                        stackState.push(canshu);
+                        if (!stack2.peek().equals("#"))
+                            stack2.pop();
+                        System.out.println("移入：" + input);
+                        if (!input.equals("#"))
+                            stack1.push(input);
+                    } else if (canshu < 0) {
+                        String wf = wenfaList.get(-canshu);
 //                    System.out.println("选择规约的项目为："+wf);
-                    String[] frontAndEnd = wf.split("#");
-                    int endLength = frontAndEnd[1].trim().length();
+                        String[] frontAndEnd = wf.split("#");
+                        int endLength = frontAndEnd[1].trim().length();
 //                    System.out.println(endLength);
-                    for(int j=0;j<endLength;j++){
-                        stackState.pop();
-                        stack1.pop();
-                    }
+                        for (int j = 0; j < endLength; j++) {
+                            stackState.pop();
+                            stack1.pop();
+                        }
 //                    System.out.println(frontAndEnd[0]);
 //                    System.out.println(stackState.size());
-                    stack1.push(frontAndEnd[0]);
-                    Integer f1 = gotoMap.get(stack1.peek()).get(stackState.peek());
+                        stack1.push(frontAndEnd[0]);
+                        Integer f1 = gotoMap.get(stack1.peek()).get(stackState.peek());
 //                    System.out.println(f1);
-                    stackState.push(f1);
+                        stackState.push(f1);
 
-                    System.out.println("规约："+frontAndEnd[0]+"->"+frontAndEnd[1]);
+                        System.out.println("规约：" + frontAndEnd[0] + "->" + frontAndEnd[1]);
 //                    System.out.println(stackState.peek());
-                }else if(canshu==999){
-                    System.out.println("accept!!!");
-                    break;
-                }else{
-                    System.out.println("error!!!");
-                    break;
-                }
-            }while (!stack1.peek().equals("#"));
+                    } else if (canshu == 999) {
+                        System.out.println("accept!!!");
+                        break;
+                    } else {
+                        System.out.println("error!!!");
+                        break;
+                    }
+                } while (!stack1.peek().equals("#"));
+                System.out.println("==========");
+            }
         }
     }
 }
